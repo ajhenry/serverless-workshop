b533219c3bcf3a645c8e7cc3277dfddbfe386c7e
Nick Smider <nicksmider@gmail.com>
2020-10-09 17:43:48 -0400

    extend tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ['standard', 'prettier', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
