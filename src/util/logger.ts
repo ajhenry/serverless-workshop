dd77e0c86221bee8202a9f036ae202ba12fdd2b1
Nick Smider <nicksmider@gmail.com>
2020-10-20 19:26:53 -0400

    updates for deployment

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,12 +5,9 @@ export const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
   ],
 });
 
